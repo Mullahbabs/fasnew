af667d176de997b74e0cd332c16bd8ea2418823e
mullahbabs <bennybeshel@gmail.com>
2025-08-23 05:10:43 +0100

    trash

--- script.js
+++ script.js
@@ -1236,7 +1236,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         </div>
                         <button class="remove-item" data-id="${
                           item.id
-                        }">Remove</button>
+                        }"><i class="fas fa-trash"></i></button>
                     `;
       cartItemsContainer.appendChild(cartItem);
     });
